61f6fbbdf58c59b852150375f969ed355e007fef
Nicholas <nschenk30@gmail.com>
2021-11-14 22:15:07 -0500

    JS files added

---
+++ js/blog_js.js
@@ -0,0 +1,9 @@
+function showPostComment(){
+    document.getElementById('postComment').style.visibility = 'visible';
+}
+
+function commentPost(){
+    document.getElementById('postComment').style.visibility = 'collapse';
+    document.getElementById('commentPost').appendChild('commentOne')
+    
+}
